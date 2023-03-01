61a64f0ad936b88da79b5f354ed8fc28465609f5
My Secont Github Repo <hmsadikurrahman120@gmail.com>
2023-03-02 01:54:12 +0600

    complete to three practic problem

--- practice-1.js
+++ practice-1.js
@@ -26,6 +26,24 @@ ${student.subjects[1]}
 // console.log(template)
 
 
-// practice 3
+// practice 3_1
 const noParameter = () => 89;
 // console.log(noParameter())
+
+
+// practice 3_2
+const oneParameter = num => num / 17
+// console.log(oneParameter(40))
+
+// practice 3_3
+const twoParameter = (num1, num2) => (num1 + num2) / 2;
+// console.log(twoParameter(5, 5))
+
+// practice 3_4
+const multiLine = (num1, num2) => {
+    const first = num1 + 7;
+    const second = num2 + 7;
+    const total = first + second
+    return total
+}
+console.log(multiLine(10, 10))
